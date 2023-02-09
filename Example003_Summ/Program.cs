78578d5f284dabe02ed965c1376fc7e2ef6b619b
Igor Chaykov <ingvorghost@gmail.com>
2023-02-09 22:17:26 +0700

    где-то напутал

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -1,6 +1,7 @@
-﻿int numberA = new Random().Next(1, 10); // рандомное число от 1 до 9 включ.
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿
+        int numberA = new Random().Next(1, 10); // рандомное число от 1 до 9 включ.
+        Console.WriteLine(numberA);
+        int numberB = new Random().Next(1, 10);
+        Console.WriteLine(numberB);
+        int result = numberA + numberB;
+        Console.WriteLine(result)
