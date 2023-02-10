ec8aaca1068ad0ec7c27de776d11b678f4ce34e0
Igor Chaykov <ingvorghost@gmail.com>
2023-02-10 23:49:13 +0700

    even or odd

---
+++ SeminarExample01/Homework/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number to check: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Yes, this number is even!");
+}
+else
+{
+    Console.WriteLine("No, this number is odd!");
+}
+
+
