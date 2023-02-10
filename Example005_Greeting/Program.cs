e7a225863e841b06f67dbfc44d6f0b1f5def2846
Igor Chaykov <ingvorghost@gmail.com>
2023-02-10 10:26:48 +0700

    example005 greeting

---
+++ Example005_Greeting/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "игорь")
+{
+    Console.WriteLine("Здорова, братан!");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(username);
+}
