e2da0313f8eb14d1e9b46944ddbea78f6f072ad5
Igor Chaykov <ingvorghost@gmail.com>
2023-02-11 01:19:50 +0700

    even numbers in a segment

---
+++ SeminarExample01/Homework/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (N < count)
+{
+    Console.WriteLine("you need to enter a number greater than 1");
+}
+else
+{
+if (count == N)
+    Console.WriteLine("there are no even numbers in this segment");
+else
+{
+while (count != N)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
+if (count % 2 == 0)
+{
+  Console.WriteLine(count);  
+}
+}
+}
