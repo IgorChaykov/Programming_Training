b24cbaef758949036d5e48a68945e982ae4156b5
Igor Chaykov <ingvorghost@gmail.com>
2023-02-10 22:50:20 +0700

    bigger & smaller

---
+++ SeminarExample01/Homework/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("This number is bigger: ");
+    Console.WriteLine(a);
+    Console.Write("This number is smaller: ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("This number is bigger: ");
+    Console.WriteLine(b);
+    Console.Write("This number is smaller: ");
+    Console.WriteLine(a);
+}
