1c621504382fe5735069f7e578700f0d997e84f0
Igor Chaykov <ingvorghost@gmail.com>
2023-02-10 23:29:16 +0700

    The greater of three numbers

---
+++ SeminarExample01/Homework/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I need three numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Bigger number is  ");
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine(a);
+    }
+    else 
+    {
+        Console.WriteLine(c);
+    }
+}
+else 
+{
+  if (b > c)
+    {
+        Console.WriteLine(b);
+    }
+    else 
+    {   
+        Console.WriteLine(c);
+    }  
+}
