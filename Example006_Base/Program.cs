78b6063f4f71ac818eccd3f97a2f6e0650d12554
Igor Chaykov <ingvorghost@gmail.com>
2023-02-10 10:49:35 +0700

    example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = 3;
+int B = 5;
+int C = 1;
+int D = 9;
+int E = 2;
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+if (D > max) max = D;
+if (E > max) max = E;
+
+Console.Write(max);
