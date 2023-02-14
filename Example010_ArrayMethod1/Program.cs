1d18d068b98958ba31f44cdc3d4359e35d722e7a
Igor Chaykov <ingvorghost@gmail.com>
2023-02-14 10:14:46 +0700

    индекс искомого элемента массива

---
+++ Example010_ArrayMethod1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] Array = { 6, 5, 19, 75, 26, 62, 49, 22 };
+
+int n = Array.Length;
+int find = 49;
+
+int index = 0;
+
+while ( index < n )
+{
+    
+    if(Array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
