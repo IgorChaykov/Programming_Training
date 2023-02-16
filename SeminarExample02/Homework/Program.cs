60e08a10c21c1e6348740ebdcae446ff8fc0c5fa
Igor Chaykov <ingvorghost@gmail.com>
2023-02-16 15:46:01 +0700

    Task 1

---
+++ SeminarExample02/Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter three-digit number ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100 || num > 999)
+{
+    Console.WriteLine($"{num} is not a three-digit number");
+}
+else 
+{
+    num = num / 10;
+    num = num % 10;
+    Console.WriteLine(num);
+}
