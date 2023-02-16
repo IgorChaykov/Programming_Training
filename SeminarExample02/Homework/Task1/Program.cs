20e221700072b6ef74f83dc56f2ce0422996050f
Igor Chaykov <ingvorghost@gmail.com>
2023-02-16 16:41:15 +0700

    Task 1

---
+++ SeminarExample02/Homework/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100 || num > 999)
+{
+    Console.WriteLine($"{num} is not three-digit number");
+}
+else
+{
+    num = num / 10; num = num % 10;
+    Console.WriteLine(num);
+}
+
+
