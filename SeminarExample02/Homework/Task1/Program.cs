e3c0cac71731588e40a9b92f2631b386fed7839e
Igor Chaykov <ingvorghost@gmail.com>
2023-02-16 16:14:12 +0700

    Task 1

--- SeminarExample02/Homework/Task1/Program.cs
+++ SeminarExample02/Homework/Task1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter three-digit number ");
+﻿Console.WriteLine("Enter three-digit number ");
 int num = Convert.ToInt32(Console.ReadLine());
 if (num < 100 || num > 999)
 {
